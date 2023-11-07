87724a751057892e177a07f38233f4bd483cf660
mollsilita <mollyvb14@yahoo.com>
2023-11-06 20:28:03 -0600

    Assignment 3 MVC

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -28,10 +28,13 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+
+        tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
         tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+
     }
 
     @GetMapping(value = "")
